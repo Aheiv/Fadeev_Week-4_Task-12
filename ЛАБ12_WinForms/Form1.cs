94c8628bc59514a55e75d60493122acbcc0e0682
DaniilF2323@outlook.com <DaniilF2323@outlook.com>
2022-12-21 21:46:41 +0300

    Добавьте файлы проекта.

---
+++ ЛАБ12_WinForms/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace ЛАБ12_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+            textBox4.Text = "";
+            string pattern = textBox2.Text;
+            string text = textBox1.Text;
+            RegExp A = new RegExp(text, pattern);
+            A.Exist(textBox5);
+            A.ShowMatches(textBox3);
+            A.Text = A.DeleteMatches(textBox4);
+            A.TextCheck(out bool check);
+            if (check == true) textBox7.Text = "Нет";
+            else textBox7.Text = "Да";
+            label6.Visible = true;
+            button2.Visible = true;
+            numericUpDown1.Visible = true;
+            textBox6.Visible = true;
+            textBox1.ReadOnly = true;
+            textBox2.ReadOnly = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int index = int.Parse(numericUpDown1.Text);
+            string pattern = textBox2.Text;
+            string text = textBox4.Text;
+            RegExp A = new RegExp(text, pattern);
+            A.CheckType(index, textBox6);
+            label8.Visible = true;
+            textBox8.Visible = true;
+            button3.Visible = true;
+            Regex regex = new Regex(pattern);
+            textBox10.Text = (A - regex);
+            textBox10.Visible = true;
+            label10.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string pattern = textBox2.Text;
+            string text = textBox4.Text;
+            RegExp A = new RegExp(text, pattern);
+            string str = textBox8.Text;
+            A.AddText(ref str, textBox9);
+            textBox9.Visible = true;
+            label9.Visible = true;
+            textBox11.Visible = true;
+            label11.Visible = true;
+            textBox12.Visible = true;
+            label12.Visible = true;
+            string d = (RegExp)A;
+            textBox11.Text = d;
+            A = (string)d;
+            textBox12.Text = textBox11.Text;
+        }
+    }
+    class RegExp
+    {
+        public RegExp(string txt, string pattern)
+        {
+            r = new Regex(pattern.ToLower());
+            text = txt;
+        }
+        private Regex r;
+        private string text;
+
+        public bool Exist(TextBox textBox)
+        {
+            if (r.IsMatch(text.ToLower()))
+            {
+                textBox.Text = "Да";
+                return r.IsMatch(text);
+            }
+            else
+            {
+                textBox.Text = "Нет";
+                return false;
+            }
+        }
+
+        public void ShowMatches(TextBox textBox)
+        {
+            MatchCollection m = r.Matches(text.ToLower());
+            foreach (Match x in m)
+                textBox.Text += $"{x.Value} ";
+        }
+
+        public string DeleteMatches(TextBox textBox)
+        {
+            MatchCollection m = r.Matches(text.ToLower());
+            string s = text.ToLower();
+            foreach (Match x in m)
+            {
+                int i = s.IndexOf(x.Value);
+                int l = x.Value.Length;
+                s = s.Remove(i, l);
+                text = text.Remove(i, l);
+            }
+            textBox.Text += text;
+            return s;
+        }
+
+        public Regex R
+        {
+            get { return r; }
+            set { r = value; }
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        public bool TextCheck(out bool check)
+        {
+            if (text != string.Empty)
+            {
+                check = true;
+                return check;
+            }
+            else
+            {
+                check = false;
+                return check;
+            }
+        }
+
+
+        public object CheckType(int n, TextBox textBox)
+        {
+            if (n == 0)
+            {
+                textBox.Text = R.ToString();
+                return new Regex("");
+            }
+            else if (n == 1)
+            {
+                textBox.Text = text;
+                return "string";
+            }
+            else textBox.Text = "ОШИБКА";
+            return null;
+        }
+
+        public object this[int i, System.Windows.Forms.TextBox f]
+        {
+
+            set
+            {
+
+                var result = CheckType(i, f);
+                if (result.GetType() == typeof(Regex))
+                {
+                    R = (Regex)value;
+                }
+                else
+                {
+                    text = value.ToString();
+                }
+            }
+
+            get
+            {
+                var result = CheckType(i, f);
+                if (result.GetType() == typeof(Regex))
+                {
+                    return R;
+                }
+                else
+                {
+                    return text;
+                }
+
+            }
+        }
+        public static string operator -(RegExp obj, Regex r)
+        {
+            MatchCollection m = r.Matches(obj.text.ToLower());
+            string s = obj.text.ToLower();
+            string ttt = obj.text;
+            foreach (Match x in m)
+            {
+                int i = s.IndexOf(x.Value);
+                int l = x.Value.Length;
+                s = s.Remove(i, l);
+                ttt = ttt.Remove(i, l);
+            }
+            string g = ttt;
+            obj.text = ttt;
+            return obj.text;
+        }
+
+        public string AddText(ref string str, TextBox textBox)
+        {
+            textBox.Text = text + str;
+            return text;
+        }
+
+        public static implicit operator string(RegExp obj)
+        {
+            return obj.R.ToString();
+        }
+
+        public static implicit operator RegExp(string Text)
+        {
+            RegExp reg = new RegExp(Text, " ");
+            return reg;
+        }
+    }
+}
---
+++ ЛАБ12_УП/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ЛАБ12_УП
+{
+    class RegExp
+    {
+
+        public RegExp(string pattern, string txt)
+        {
+            r = new Regex(pattern.ToLower());
+            text = txt;
+        }
+        private Regex r;
+        private string text;
+
+        public bool Exist()
+        {
+            if (r.IsMatch(text.ToLower()))
+            {
+                Console.WriteLine("Текст содержит фрагменты, соответствующие шаблону");
+                return r.IsMatch(text);
+            }
+            else
+            {
+                Console.WriteLine("Текст не содержит фрагменты, соответствующие шаблону");
+                return false;
+            }
+        }
+
+        public void ShowMatches()
+        {
+            MatchCollection m = r.Matches(text.ToLower());
+            foreach (Match x in m)
+                Console.Write(x.Value + " ");
+        }
+
+        public string DeleteMatches()
+        {
+            MatchCollection m = r.Matches(text.ToLower());
+            string s = text.ToLower();
+            string ttt = text;
+            foreach (Match x in m)
+            {
+                int i = s.IndexOf(x.Value);
+                int l = x.Value.Length;
+                s = s.Remove(i, l);
+                ttt = ttt.Remove(i, l);
+            }
+            Console.WriteLine(ttt);
+            text = ttt;
+            return text;
+        }
+
+        public Regex R
+        {
+            get { return r; }
+            set { r = value; }
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        public bool TextCheck(out bool check)
+        {
+            if (text != string.Empty)
+            {
+                check = true;
+                return check;
+            }
+            else
+            {
+                check = false;
+                return check;
+            }
+        } 
+
+       
+        public object CheckType(int n)
+        {
+            if (n == 0)
+            {
+                Console.WriteLine(R);
+                return new Regex("");
+            }
+            else if (n == 1)
+            {
+                Console.WriteLine(text);
+                return "string";
+            }
+            else Console.WriteLine("ОШИБКА");
+            return null;
+        }
+
+
+
+        public object this[int i]
+        {
+
+            set
+            {
+
+                var result = CheckType(i);
+                if (result.GetType() == typeof(Regex))
+                {
+                    R = (Regex)value;
+                }
+                else
+                {
+                    text = value.ToString();
+                }
+            }
+
+            get
+            {
+                var result = CheckType(i);
+                if (result.GetType() == typeof(Regex))
+                {
+                    return R;
+                }
+                else
+                {
+                    return text;
+                }
+
+            }
+        }
+
+        public string AddText(ref string str)
+        {
+            Console.WriteLine(text + str);
+            return text;
+        }
+        
+        public static string operator -(RegExp obj, Regex r)
+        {
+            MatchCollection m = r.Matches(obj.text.ToLower());
+            string s = obj.text.ToLower();
+            string ttt = obj.text;
+            foreach (Match x in m)
+            {
+                int i = s.IndexOf(x.Value);
+                int l = x.Value.Length;
+                s = s.Remove(i, l);
+                ttt = ttt.Remove(i, l);
+            }
+            obj.text = ttt;
+            return obj.text;
+        }
+
+        public static implicit operator string(RegExp obj)
+        {
+            return obj.R.ToString();
+        }
+
+        public static implicit operator RegExp(string Text)
+        {
+            RegExp reg = new RegExp(Text, "");
+            return reg;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваш текст: ");
+            string text = Console.ReadLine();
+            Console.Write("Введите регулярное выражение: ");
+            string pattern = Console.ReadLine();
+            RegExp A = new RegExp(pattern, text);
+            A.Exist();
+            Console.WriteLine("Фрагменты, не соответствующие шаблону:");
+            A.ShowMatches();
+            Console.WriteLine("\nОставшийся текст, соответствующий шаблону:");
+            A.Text = A.DeleteMatches();
+            Console.Write("Введите индекс: ");
+            int index = int.Parse(Console.ReadLine());
+            A.CheckType(index);
+            Console.Write("Этот текст пустой? Ответ: ");
+            A.TextCheck(out bool check);
+            if (check == true) Console.WriteLine("Нет");
+            else Console.WriteLine("Да");
+            Console.WriteLine("Удаление всех фрагментов, соответствующих значению шаблона");
+            Regex regex = new Regex(pattern);
+            Console.WriteLine(A - regex);
+            Console.Write("Введите строку, которую надо добавить в текст: ");
+            string str = Console.ReadLine();
+            A.AddText(ref str);
+            string d = (RegExp)A;
+            Console.WriteLine("Преобразование шаблона в string: {0}", d);
+            A = (string) d;
+            Console.WriteLine("Преобразование шаблона в Regex: {0}", new Regex (A));
+        }
+    }
+}
